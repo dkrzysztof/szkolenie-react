28b03c795fbcbdea5bb8e54e0364d3982d99770b
Krzysztof <dydakkrzysztof@gmail.com>
2020-06-23 13:14:29 +0200

    Updates

--- src/App/api/paints/PaintsEndpoints.ts
+++
@@ -1,6 +0,0 @@
-import { requests } from "../agent";
-import { GET_PAINTS_STATISTICS_MOST_USED, IMostUsedPaintsResponse } from "./PaintsInterface";
-
-export const PaintsEndpoints = {
-    getMostUsedPaints: (): Promise<IMostUsedPaintsResponse> => requests.get(GET_PAINTS_STATISTICS_MOST_USED, {}, {}),
-};
